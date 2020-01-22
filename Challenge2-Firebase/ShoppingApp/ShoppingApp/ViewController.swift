38f5f130e4d651a7d5a7e7a68152c75d35323597
Dlee4428 <44447609+Dlee4428@users.noreply.github.com>
2020-01-21 19:24:26 -0500

    Firebase-Challenge-Update

---
+++ Challenge2-Firebase/ShoppingApp/ShoppingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShoppingApp
+//
+//  Created by Dong Hyeong Lee on 2020-01-21.
+//  Copyright © 2020 Dong Hyeong Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+

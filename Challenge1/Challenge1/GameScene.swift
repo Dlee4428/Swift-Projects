8848d30b696c882d6e8d8e5ca8ff28f0aa1898f1
Sarfaraz Syed <38136379+SunHorizon@users.noreply.github.com>
2020-02-01 17:55:02 -0800

    Added title(not done)

--- Challenge1/Challenge1/GameScene.swift
+++ Challenge1/Challenge1/GameScene.swift
@@ -12,27 +12,40 @@ class GameScene: SKScene {
     override func didMove(to view: SKView) {
         // Background features
         backgroundColor = SKColor.black
-        let background = SKSpriteNode(imageNamed: "background1")
+        let background = SKSpriteNode(imageNamed: "background3")
         addChild(background)
         background.position = CGPoint(x: size.width / 2, y: size.height / 2)
+        background.zRotation = 1.58
+        background.zPosition = -1
+        background.setScale(1.6)
+        
+        let titleText = UITextView(frame: CGRect(x: 750.0, y: 10, width: 50, height: 300.0))
+        
+        
+        titleText.backgroundColor = .yellow
+        titleText.text = "Hello World"
+        titleText.textColor = .black
+        
+        view.addSubview(titleText)
+        
         
         // Challenge 1: Zombie position
         // Player(Zombie) Features
-        let zombie = SKSpriteNode(imageNamed: "zombie1")
-        zombie.position = CGPoint(x: -500, y: -300)
+        //let zombie = SKSpriteNode(imageNamed: "zombie1")
+       // zombie.position = CGPoint(x: -500, y: -300)
         
         // place the background behind other stuff
-        background.zPosition = -1
+        
         
         // place the zombie in front
-        zombie.zPosition = 1
+        //zombie.zPosition = 1
         
         
         // Challenge 2: Scale up to 2x
-        background.setScale(0.8)
-        zombie.setScale(2.0)
+       
+        //zombie.setScale(2.0)
         
         // Challenge 3: Position value change if you make zombie a child of background
-        background.addChild(zombie)
+        //background.addChild(zombie)
     }
 }
